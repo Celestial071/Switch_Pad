876dca898a403426fe13dcd3449303a21d557ef8
Celestial071 <157342628+Celestial071@users.noreply.github.com>
2024-02-16 23:53:34 +0545

    Add files via upload

--- arduino_code.ino
+++ arduino_code.ino
@@ -9,49 +9,60 @@
 #define BIT7 0b00000010
 #define BIT8 0b00000001
 
-
 uint8_t data = 0x0;
-uint8_t button[] = {2,3,4,5};
+uint8_t prevData = 0xFF;
+uint8_t toggle = 0x0;
+uint8_t button[] = {2, 3, 4, 5};
 bool handshake_accept = false;
+bool check = false; // Used for toggling BIT8
 
-/*void printBinary(uint8_t d){
-  for (int i = 7; i >= 0; i--) {
-        Serial.print((d >> i) & 1);
-    }
-    Serial.println("");
-}*/
 void setup() {
   Serial.begin(9600);
-  
-
-  // Wait a bit for the serial port to establish connection with the PC
   delay(2000);
+  // Initialize button pins as inputs
+  for (int i = 0; i < 4; i++) {
+    pinMode(button[i], INPUT);
+  }
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
 }
 
 void loop() {
-  if(!handshake_accept){
-    Serial.println("HANDSHAKE");  // Send the handshake message
-    while(Serial.available() == 0) {
-      ; // Do nothing until we get a response
+  if (!handshake_accept) {
+
+    Serial.println("HANDSHAKE");
+    while (Serial.available() == 0) {
+      ; // Wait for response
     }
-    String response = Serial.readStringUntil('\n'); // Read the response
-    if(response.startsWith("ACK")) {
-      Serial.println("Handshake received!");
+    String response = Serial.readStringUntil('\n');
+    if (response.startsWith("ACK")) {
       handshake_accept = true;
     }
-  }else{
-    //encoding the data in the format I want;;;
-    if(digitalRead(button[0]))  data |= BIT1;
-    if(digitalRead(button[1]))  data |= BIT8;
-    if(digitalRead(button[2]))  data |= BIT2;
-    if(digitalRead(button[3]))  data |= BIT3;
+  } else {
+    data = 0x0;
+    if (digitalRead(button[0])) data |= BIT1;
+    if (digitalRead(button[2])) data |= BIT2;
+    if (digitalRead(button[3])) data |= BIT3;
+    if (analogRead(A1) < 250) data |= BIT5;
+    if (analogRead(A1) > 750) data |= BIT4;
+    if (analogRead(A0) < 250) data |= BIT7;
+    if (analogRead(A0) > 750) data |= BIT6;
 
-    if(analogRead(A1) < 250)  data |= BIT5;
-    if(analogRead(A1) > 750)  data |= BIT4;
-    if(analogRead(A0) < 250)  data |= BIT7;
-    if(analogRead(A0) > 750)  data |= BIT6;
+    if (digitalRead(button[1])) {
+      if (!check) {
+        toggle ^= BIT8; // Toggle the state of BIT8
+        check = true;
+      }
+    } else {
+      check = false;
+    }
 
-    Serial.write(data);
+    data |= toggle;
+
+    if (data != prevData) {
+      Serial.write(data);
+      prevData = data;
+    }
   }
-  data = 0x0;
+  delay(10);
 }
